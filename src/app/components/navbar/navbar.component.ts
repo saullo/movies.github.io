ed267d50ac6fbf7eaf3a30bd2af1b1c42389123b
Saullo Bretas Silva <saullo.silva303@gmail.com>
2020-03-08 12:10:47 -0300

    Fixed menu close
    Close menu when click outside and on change route

--- src/app/components/navbar/navbar.component.ts
+++ src/app/components/navbar/navbar.component.ts
@@ -1,15 +1,23 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ElementRef } from '@angular/core';
+import { Router, Event, NavigationStart } from '@angular/router';
 
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.scss']
+  styleUrls: ['./navbar.component.scss'],
+  host: {
+    '(document:click)': 'onClick($event)',
+  },
 })
 export class NavbarComponent implements OnInit {
   menuOpen: boolean
 
-  constructor() { }
+  constructor(private elementRef: ElementRef) { }
 
   ngOnInit(): void {
+  }
+
+  onClick(event) {
+    if (!this.elementRef.nativeElement.contains(event.target)) this.menuOpen = false
   }
 }
