7211e5cd613161a2e491ceeed18fe66fb57faffe
Saullo Bretas Silva <saullo.silva303@gmail.com>
2020-03-08 00:00:07 -0300

    Fixed routing in GithubPages
    Start using hash in router

--- src/app/views/app/app.component.ts
+++ src/app/views/app/app.component.ts
@@ -11,9 +11,7 @@ export class AppComponent {
 
   constructor(private router: Router) {
     this.router.events.subscribe((event: Event) => {
-      if (event instanceof NavigationStart) {
-        window.scrollTo(0, 0)
-      }
+      if (event instanceof NavigationEnd) window.scrollTo(0, 0)
     })
   }
 }
